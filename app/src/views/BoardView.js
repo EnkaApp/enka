c4118fe18b3155152ba7277f043df5e08e0f4421
keithGrout <keiwi.g@gmail.com>
2014-06-13 17:32:20 -0700

    returning y coordinates

--- app/src/views/BoardView.js
+++ app/src/views/BoardView.js
@@ -10,7 +10,7 @@ define(function(require, exports, module) {
   var StateModifier = require('famous/modifiers/StateModifier');
   var GridLayout    = require('famous/views/GridLayout');
 
-  var boardDimensions = [5,7];
+  var boardDimensions = [5, 7];
 
   function _createBackground() {
     var bgSurface = new Surface({
@@ -22,43 +22,39 @@ define(function(require, exports, module) {
     this.add(bgSurface);
   }
 
+  function getY(index){
+    var columns = boardDimensions[0];
+    var rows = boardDimensions[1];
+    var x = index;
+    for(var row = 0; row < rows; row++){
+      if(x < columns){
+        return row;
+      }else if( (x >= (columns * row)) && x < ( columns * (row + 1) )){
+        return row;
+      } 
+    }
+  }
+
+  function getX(index){
+    var columns = boardDimensions[0];
+    return index % columns;
+  }
 
   function _createSurfaces() {
     var size = boardDimensions[0] * boardDimensions[1];
     var surfaces = [];
-    var x = 0;
-    var y = 0;
-
-    function getX(index){
-      var columns = boardDimensions[0];
-      var rows = boardDimensions[1];
-      var x = index;
-      for(var row = 0; row < rows; row++){ // x = 5, row = 1, columns = 5, rows = 7
-        if(x < columns){
-          console.log(row);
-          return row;
-        }else if( (x >= (columns * row)) && x < ( columns * (row + 1) )){
-          console.log(row);
-          return row;
-        }
-        
-      }
-   
-    }
-
 
     for(var i = 0; i < size; i++){
-
-
       surfaces.push(new Surface({
-        content: 'x: ' + getX(i),
-        size: undefined,
+        content: '(' + getX(i) + ', ' + getY(i) + ')',
+        x: getX(i),
+        y: getY(i),
+        size: [undefined, undefined],
         properties: {
           color: 'black',
           backgroundColor: 'hsl(100, 100%, 100%)',
           textAlign: 'center'
         }
-
       }));
     }
 
@@ -73,8 +69,6 @@ define(function(require, exports, module) {
     var surfaces = _createSurfaces();
     grid.sequenceFrom(surfaces);
     this.add(grid);
-
-
   }
 
   function BoardView() {
