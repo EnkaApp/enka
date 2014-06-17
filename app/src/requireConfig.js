0da81a2df2e3c474a622e65c577f5da362a383f8
dremonkey <a.deutmeyer@gmail.com>
2014-06-16 21:39:56 -0700

    added lodash

--- app/src/requireConfig.js
+++ app/src/requireConfig.js
@@ -1,12 +1,16 @@
 /*globals require*/
 require.config({
     shim: {
-
+      
     },
     paths: {
         famous: '../lib/famous',
         requirejs: '../lib/requirejs/require',
-        almond: '../lib/almond/almond'
-    }
+        almond: '../lib/almond/almond',
+        lodash: '../lib/lodash/dist/lodash.compat'
+    },
+    packages: [
+
+    ]
 });
 require(['main']);
