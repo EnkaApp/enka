c2e2a64d48a91c6d5808f5d521b1742e56129cc5
dremonkey <a.deutmeyer@gmail.com>
2014-06-20 13:46:12 -0700

    modified GridController to store more info internallly

--- app/src/GridController.js
+++ app/src/GridController.js
@@ -1,14 +1,17 @@
 
 define(function(require, exports, module) {
-  var columns;
-  var rows;
-
   var StateModifier = require('famous/modifiers/StateModifier');
   var Transform     = require('famous/core/Transform');
   var RenderNode    = require('famous/core/RenderNode');
-  var PieceView     = require('./views/PieceView');
+  var OptionsManager = require('famous/core/OptionsManager');
+  
+  // ## View
+  var PieceView     = require('./views/PieceView'); // deprecated
 
-  function getY(index) {
+  function _getY(index) {
+    var columns = this.options.columns;
+    var rows = this.options.rows;
+
     for (var row = 0; row < rows; row++) {
       if (index < columns) {
         return row;
@@ -18,27 +21,61 @@ define(function(require, exports, module) {
     }
   }
 
-  function getX(index) {
+  function _getX(index) {
+    var columns = this.options.columns;
+
     return index % columns;
   }
 
-  function GridController(dimensions) {
-    if (!dimensions) dimensions = [];
-    this._state = [];
-    createGrid.call(this, dimensions);
+  function _createGrid() {
+    var length = this.options.rows * this.options.columns;
 
-  }
-
-  function createGrid(dimensions) {
-    if (!dimensions) dimensions = [];
-    columns = dimensions[0] || GridController.DEFAULT_OPTIONS.columns;
-    rows = dimensions[1] || GridController.DEFAULT_OPTIONS.rows;
-    var length = rows * columns;
     for (var i = 0; i < length; i++) {
       this._state.push(null);
     }
-
   }
+
+  function GridController(options) {
+    this._state = [];
+    this._dimensions = [];
+
+    this.options = Object.create(this.constructor.DEFAULT_OPTIONS);
+    this._optionsManager = new OptionsManager(this.options);
+
+    if (options) this.setOptions(options);
+
+    this._dimensions = [this.options.rows, this.options.columns];
+    this._cellSize = this.getCellSize();
+
+    _createGrid.call(this);
+  }
+
+  GridController.DEFAULT_OPTIONS = {
+    rows: 7,
+    columns: 5,
+    viewSize: window.innerWidth
+  };
+
+  /**
+   * Look up options value by key
+   * @method getOptions
+   *
+   * @param {string} key key
+   * @return {Object} associated object
+   */
+  GridController.prototype.getOptions = function getOptions() {
+      return this._optionsManager.value();
+  };
+
+  /*
+   *  Set internal options.
+   *
+   *  @method setOptions
+   *  @param {Object} options
+   */
+  GridController.prototype.setOptions = function setOptions(options) {
+      this._optionsManager.patch(options);
+  };
 
   // Deprectated
   // Moved to PieceGenerator
@@ -50,25 +87,45 @@ define(function(require, exports, module) {
   // Deprectated
   // Use getCellSize instead
   GridController.prototype.getPieceSize = function(viewSize) {
-    return this.getCellSize(viewSize);
+    this.options.viewSize = viewSize;
+    return this.getCellSize();
   };
 
-  GridController.prototype.getCellSize = function(viewSize) {
-    var pSize =  viewSize[0] / columns;
-    return [pSize, pSize];
+  // 
+  GridController.prototype.getCellSize = function() {
+    var columns = this.options.columns;
+    var viewSize = this.options.viewSize;
+    var size =  viewSize[0] / columns;
+
+    return [size, size];
   };
 
+  // GridController.prototype.getXYCoordsFromGridCoords = function(coords) {
+  //   this.pieceSize
+  // }
+
+  /**
+   * @param {number} index
+   * @param {number} pieceSize DEPRECATED
+   */
   GridController.prototype.getXYCoords = function(index, pieceSize) {
+    var size;
+
     // return xy coords
     var xyCoords = [];
     var coords = {
-      x: getX(index),
-      y: getY(index)
+      x: _getX.call(this, index),
+      y: _getY.call(this, index)
     };
 
-    var size = pieceSize;
-    xPix = getX(index) * size;
-    yPix = getY(index) * size;
+    if (pieceSize) {
+      size = pieceSize;
+    } else {
+      size = this._cellSize;
+    }
+
+    xPix = _getX.call(this, index) * size;
+    yPix = _getY.call(this, index) * size;
     xyCoords.push(xPix, yPix);
 
     console.log('coordinates@index ' + index + ': ' + '(' + coords.x + ', ' + coords.y + ')');
@@ -80,11 +137,6 @@ define(function(require, exports, module) {
 
   GridController.prototype.isEmpty = function(index) {
     return this._state === null;
-  };
-
-  GridController.DEFAULT_OPTIONS = {
-    rows: 7,
-    columns: 5
   };
 
   module.exports = GridController;
