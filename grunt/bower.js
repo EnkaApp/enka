7fa796d6230042d545b305edc9551b38b8b8a448
dremonkey <a.deutmeyer@gmail.com>
2014-07-07 14:24:03 -0700

    v0.1.0

--- grunt/bower.js
+++
@@ -1,9 +0,0 @@
-// Automagically wire-up installed Bower components into your RequireJS config
-module.exports = {
-  raget: {
-    rjsConfig: '<%= config.app %>/src/requireConfig.js'
-  },
-  options: {
-    exclude: ['famous', 'famous-polyfills', 'requirejs', 'almond']
-  }
-};
