d2546ba8703215b338f8a9c595e3ebc82e8feb6f
dremonkey <a.deutmeyer@gmail.com>
2014-07-07 12:49:42 -0700

    fixed grunt copy task to move images and fonts over to dist correctly

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,19 +1,36 @@
 // Copies remaining files to places other tasks can use
 module.exports = {
   dist: {
-    files: [{
-      expand: true,
-      dot: true,
-      cwd: '<%= config.app %>',
-      dest: '<%= config.dist %>',
-      src: [
-        'content/**/**.*',
-        '.htaccess',
-        'images/{,*/}*.webp',
-        // '{,*/}*.html',
-        'styles/fonts/{,*/}*.*',
-        'lib/famous/**/**.css'
-      ]
-    }]
+    files: [
+      {
+        expand: true,
+        dot: true,
+        cwd: '<%= config.app %>',
+        dest: '<%= config.dist %>',
+        src: [
+          '.htaccess',
+          'lib/famous/**/**.css'
+        ]
+      },
+
+      {
+        expand: true,
+        cwd: '<%= config.app %>/assets',
+        dest: '<%= config.dist %>',
+        src: [
+          'images/*.png',
+          'images/{,*/}*.png'
+        ]
+      },
+
+      {
+        expand: true,
+        cwd: '<%= config.app %>/assets',
+        dest: '<%= config.dist %>',
+        src: [
+          'fonts/*'
+        ]
+      }
+    ]
   }
 };
